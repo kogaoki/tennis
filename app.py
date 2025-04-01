28e4b666f4e587b9408443e115a9b2dd35573683
kogaoki <music.lp41@gmail.com>
2025-04-02 00:04:37 +0900

    Update app.py

--- app.py
+++ app.py
@@ -116,10 +116,14 @@ if st.button("スコアシートPDFをダウンロード"):
         pdf_template = fitz.open(stream=response.content, filetype="pdf")
         output_pdf = fitz.open()
 
-        font_url = "https://raw.githubusercontent.com/kogaoki/tennis/main/ipag.ttf"
-        font_response = requests.get(font_url)
-        font_bytes = BytesIO(font_response.content)
-        custom_font = fitz.Font(fontfile=font_bytes, fontname="ipagothic")
+font_url = "https://raw.githubusercontent.com/kogaoki/tennis/main/ipag.ttf"
+font_response = requests.get(font_url)
+
+with tempfile.NamedTemporaryFile(delete=False, suffix=".ttf") as tmp_font_file:
+    tmp_font_file.write(font_response.content)
+    font_path = tmp_font_file.name
+
+custom_font = fitz.Font(fontfile=font_path, fontname="ipagothic")
 
         coords = {
             "no1": (92, 188), "team1": (213, 188), "p1_1": (187, 221), "p1_2": (187, 257),
