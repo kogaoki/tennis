2ba00deb9a9445fdd10b7a16b558cf29761f2d7b
kogaoki <music.lp41@gmail.com>
2025-04-02 01:08:16 +0900

    Update app.py

--- app.py
+++ app.py
@@ -178,19 +178,19 @@ if st.button("スコアシートPDFをダウンロード"):
             height = A4[1]
 
             def draw_text(x, y, text, center=False):
-                c.setFont("CustomJP", 12)
+                c.setFont("CustomJP", 10)
                 if center:
                     text_width = c.stringWidth(text, "CustomJP", 12)
                     x -= text_width / 2
                 c.drawString(x, height - y, text)
 
-            draw_text(coords["team1"][0] - 20, coords["team1"][1], team1, center=True)
+            draw_text(coords["team1"][0] - 10, coords["team1"][1], team1, center=True)
             draw_text(coords["p1_1"][0], coords["p1_1"][1], p1_1)
             if p1_2:
                 draw_text(coords["p1_2"][0], coords["p1_2"][1], p1_2)
             draw_text(coords["no1"][0], coords["no1"][1], match["ペア1"])
 
-            draw_text(coords["team2"][0] - 20, coords["team2"][1], team2, center=True)
+            draw_text(coords["team2"][0] - 10, coords["team2"][1], team2, center=True)
             draw_text(coords["p2_1"][0], coords["p2_1"][1], p2_1)
             if p2_2:
                 draw_text(coords["p2_2"][0], coords["p2_2"][1], p2_2)
